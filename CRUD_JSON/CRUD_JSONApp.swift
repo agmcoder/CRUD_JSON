ad2928b656feee16e250b6e2946cd76a4019e45c
agmcoder <agmcoder@gmail.com>
2022-09-24 22:25:41 +0200

    Initial Commit

---
+++ CRUD_JSON/CRUD_JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_JSONApp.swift
+//  CRUD_JSON
+//
+//  Created by agmcoder on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
